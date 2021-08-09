66cba6749cd8f66ada8aad0d44636acb5f7bc111
Nathan Arnold <Nathan.Arnold817@gmail.com>
2021-08-09 17:10:02 -0400

    added details

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -94,5 +94,20 @@ namespace RestaurantRater.Controllers
             }
             return View(restaurant);
         }
+
+        //Get: Restaurant/Detail/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
     }
 }
